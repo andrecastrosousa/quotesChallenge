91e877ed86d1cf3174361a62276ffb317d00356a
André Sousa <andre.castro.sousa@gmail.com>
2023-11-15 20:56:09 -0100

    add: api endpoints on controller

--- src/main/kotlin/com/andrecastrosousa/controller/QuoteController.kt
+++ src/main/kotlin/com/andrecastrosousa/controller/QuoteController.kt
@@ -1,4 +1,21 @@
 package com.andrecastrosousa.controller
 
-class QuoteController {
+import com.andrecastrosousa.model.Quote
+import com.andrecastrosousa.service.QuoteService
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.QueryValue
+import java.util.concurrent.Flow.Publisher
+
+@Controller("/api/quotes")
+class QuoteController(private val quoteService: QuoteService) {
+
+    @Get
+    fun listAll(): Publisher<List<Quote>> = quoteService.listAll()
+
+    @Get("/id")
+    fun findById(id: String): Publisher<Quote> = quoteService.findById(id)
+
+    @Get("/search")
+    fun findByAuthor(@QueryValue author: String?): Publisher<List<Quote>> = quoteService.findByAuthor(author)
 }
