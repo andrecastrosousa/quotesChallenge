53f958bc14293c3621ce8eccd95cf93547b5bfea
André Sousa <andre.castro.sousa@gmail.com>
2023-11-16 09:04:37 -0100

    fix: integration tests bugs

--- src/test/kotlin/com/andrecastrosousa/controller/QuoteControllerIntegrationTest.kt
+++ src/test/kotlin/com/andrecastrosousa/controller/QuoteControllerIntegrationTest.kt
@@ -1,35 +1,38 @@
 package com.andrecastrosousa.controller
 
+import com.andrecastrosousa.model.Quote
 import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.http.client.exceptions.HttpClientResponseException
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.function.Executable
 
 @MicronautTest
 class QuoteControllerIntegrationTest(@Client("/") val client: HttpClient) {
     @Test
     fun `Should get a quote by ID`() {
-        val request: HttpRequest<Any> = HttpRequest.GET("/api/quotes/1")
-        val body = client.toBlocking().retrieve(request)
+        val e = Executable { client.toBlocking().exchange<Any, Any>(HttpRequest.GET<Any>("/")) }
 
-        println(body)
-        assertNotNull(body)
+        val thrown = assertThrows(HttpClientResponseException::class.java, e)
+        assertEquals(HttpStatus.NOT_FOUND, thrown.status)
     }
 
     @Test
     fun `Should get a list of quotes`() {
-        val request: HttpRequest<Any> = HttpRequest.GET("/api/quotes")
-        val body = client.toBlocking().retrieve(request)
+        val body = client.toBlocking().exchange<Quote, Any>(
+            HttpRequest.GET("/api/quotes"))
 
         assertNotNull(body)
     }
 
     @Test
     fun `Should get quotes by author name`() {
-        val request: HttpRequest<Any> = HttpRequest.GET("/api/quotes/search?author=Bill%20Gates")
-        val body = client.toBlocking().retrieve(request)
+        val body = client.toBlocking().exchange<List<Quote>, Any>(
+            HttpRequest.GET("/api/quotes/search?author=Bill%20Gates"))
 
         println(body)
         assertNotNull(body)
